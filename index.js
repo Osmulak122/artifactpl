6c1281fbf8d4b88eb936ecd6082f01183e8a418f
Osmulak122 <34412964+Osmulak122@users.noreply.github.com>
2018-10-11 18:21:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,38 +6,38 @@ var request = require('request');
 
 bot.on("guildMemberAdd", function(member) {
     
-    var role_rules = member.guild.roles.find('name', 'Unaccepted Rules');
+    var role_rules = member.guild.roles.find('name', 'Niepotwierdzony');
     member.addRole(role_rules)
 });
 
 
 
 bot.on("ready", function() {
-    bot.user.setActivity('m1nty.eu');
+    bot.user.setActivity('Made by Minty');
     console.log("Im Ready!");
-    global = bot.channels.find("name", "welcome").fetchMessage("455429301567750166");
+    global = bot.channels.find("name", "welcome").fetchMessage("499969753205440513");
 });
     
 bot.on('messageReactionAdd', function(reaction, users) {
     console.log("kebab")
     var Welcome = bot.channels.find("name", "welcome");
     var messageid = global;
-    var Emoji = "✅";
-    var rolenews = reaction.message.guild.roles.find("name", "Member");
+    var Emoji = bot.emojis.find("name","artifact");
+    var rolenews = reaction.message.guild.roles.find("name", "Niepotwierdzony");
     if (!reaction.message == global) return;
     if (!reaction.emoji.name == Emoji) return;
-    reaction.message.guild.members.get(users.id).addRole(rolenews);
+    reaction.message.guild.members.get(users.id).removeRole(rolenews);
 });
 
 bot.on('messageReactionRemove', function(reaction, users) {
     console.log("no kebab")
     var Welcome = bot.channels.find("name", "welcome");
     var messageid = global;
-    var Emoji = "✅";
-    var rolenews = reaction.message.guild.roles.find("name", "Member");
+    var Emoji = bot.emojis.find("name","artifact");
+    var rolenews = reaction.message.guild.roles.find("name", "Niepotwierdzony");
     if (!reaction.message == global) return;
     if (!reaction.emoji.name == Emoji) return;
-    reaction.message.guild.members.get(users.id).removeRole(rolenews);
+    reaction.message.guild.members.get(users.id).addRole(rolenews);
 });
 
 bot.on("message", function(message) {
